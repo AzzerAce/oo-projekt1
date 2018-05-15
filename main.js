30613c49a33372246fc095241d26d6d975569273
Urban Tanko <urbantanko@gmail.com>
2018-05-15 07:48:07 +0000

    navbar narjen neki mal

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ window.onscroll = function() {
 };
     
 var navbar = document.getElementById("navbar");
+var navbar_logo = document.getElementById("small-logo-img");
 var sticky = navbar.offsetTop;
 console.log(sticky);
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("navbar-fixed-top");
+    navbar_logo.classList.add("show-logo");
+    navbar_logo.classList.remove("hide-logo");
   } else {
     navbar.classList.remove("navbar-fixed-top");
+    navbar_logo.classList.remove("show-logo");
+    navbar_logo.classList.add("hide-logo");
   }
 }
 
